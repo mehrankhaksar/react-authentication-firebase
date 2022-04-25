5c922fc7774ab6aec7e7e591a528beaadf44c3cd
Mehran Khaksar <itsaboutmehran@yahoo.com>
2022-04-25 15:33:31 +0430

    setup project

---
+++ src/configs/fbConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB2hjkwiPwAxdsRiyG56wwe4j3Adhj2BUo',
+  authDomain: 'react-authentication-9ade5.firebaseapp.com',
+  projectId: 'react-authentication-9ade5',
+  storageBucket: 'react-authentication-9ade5.appspot.com',
+  messagingSenderId: '376877780183',
+  appId: '1:376877780183:web:8d3bf861c46ccd5e447492',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export { app, auth };
